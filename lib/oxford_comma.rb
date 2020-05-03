0f38915c948bcfa1c93a7906b0cac61f2137afee
Richard Hanolt <rhanolt@gmail.com>
2020-05-03 23:25:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,5 +5,11 @@ def oxford_comma(array)
     "#{array[0]} and #{array[1]}"
   elsif array.size == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    count = 0
+    oxford_comma = Array.new
+    while count < (array.size - 1)
+      oxford_comma == oxford_comma + array[count] + ","
+    end
   end
 end
