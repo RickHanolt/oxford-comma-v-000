bd97f4020195199394d930a782fc432dc68793fe
Richard Hanolt <rhanolt@gmail.com>
2020-05-03 23:39:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+def more_than_three(array)
+  count = 0
+  oxford_comma_string = ""
+  while count < (array.size - 1)
+    oxford_comma_string = oxford_comma_string + array[count] + ","
+    count += 1
+  end
+end
+
 def oxford_comma(array)
   if array.size == 1
     array[0]
@@ -6,11 +15,7 @@ def oxford_comma(array)
   elsif array.size == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
-    count = -1
-    oxford_comma = ""
-    while count < (array.size - 1)
-      count += 1
-      oxford_comma == oxford_comma + array[count] + ","
-    end
+    count = 0
+    until count 
   end
 end
